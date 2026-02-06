279d1976fcf79de58a7a1bc6b3ba05af4127ead6
trslocal-prog <trslocal@gmail.com>
2026-02-06 12:20:51 +0500

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-        open("https://www.rambler.com/");
+        open("https://www.rambler.ru/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
