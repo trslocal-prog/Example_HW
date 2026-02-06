5d5d48478c8297e33e57b9add33bd574d151d8b8
ruslantlemisov <trslocal@gmail.com>
2026-02-06 12:15:33 +0500

    HW DONE

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-        open("https://www.duckduckgo.com/");
+        open("https://www.yandex.ru/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
