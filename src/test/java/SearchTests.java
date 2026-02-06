dbeb0419531281e16c042ded5a2124afe1e1e4ed
ruslantlemisov <trslocal@gmail.com>
2026-02-06 14:59:01 +0500

    HW DONE 2pufddf

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -7,7 +7,7 @@ public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
+        $("[name=q]").setValue("sfnsndfnjksdfjksdfksbdfksbkdjfksdkfksd").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
 }
